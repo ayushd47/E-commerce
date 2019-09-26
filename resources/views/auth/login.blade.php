b57184a5ec3b07b65575697dc0aa47d09b26bac6
Ayush dangol <r1692607@gmail.com>
2019-09-26 10:23:21 +0545

    Add login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,74 @@
 @extends('layouts.main')
 
 @section('content')
-
 <section id="form">
     <!--form-->
+
     <div class="container">
+        <div class="row">
+            <div class="col-sm-9 ">
+                <div class="login-form">
+                    <!--login form-->
+                    <div class="jumbotron jumbotron-fluid">
+                        <h2>Login to your account</h2>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form-group row">
+                                <div class="col-md-9">
+                                    <input placeholder="Email Address" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-        <div class="col-sm-6 ">
-            <div class="login-form">
-                <!--login form-->
-                <h2>Login to your account</h2>
-                <form action="#">
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <span>
-                        <input type="checkbox" class="checkbox">
-                        Keep me signed in
-                    </span>
-                    <button type="submit" class="btn btn-default">Login</button>
-                </form>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <h5>{{ $message }}</h5>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-md-9">
+                                    <input placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <h5>{{ $message }}</h5>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <div class="col">
+                                        <button type="submit" class="btn btn-primary">
+                                            <a>{{ __('Login') }}</a>
+                                        </button>
+                                    </div>
+
+
+                                    <button type="submit" class="btn btn-primary">
+                                        <a href="{{ route('register') }}"> {{ __('Sign up') }}</a>
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                    @endif
+
+                                </div>
+
+
+
+
+                        </form>
+                    </div>
+                    <!--/login form-->
+                </div>
+
+
             </div>
-            <!--/login form-->
-        </div>
 
-    </div>
+        </div>
     </div>
 </section>
-<!--/form-->
 @stop
