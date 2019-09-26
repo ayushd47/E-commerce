d7218a5172103b7763b4999e734a3ba046cab14f
Ayush dangol <r1692607@gmail.com>
2019-09-26 10:27:45 +0545

    Change Index route

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -64,7 +64,7 @@
 				<div class="row">
 					<div class="col-md-4 clearfix">
 						<div class="logo pull-left">
-							<a href="index.html"><img src="{{URL::asset('assets/images/home/logo.png')}}" alt="" /></a>
+							<a href="/"><img src="{{URL::asset('assets/images/home/logo.png')}}" alt="" /></a>
 						</div>
 						<div class="btn-group pull-right clearfix">
 							<div class="btn-group">
@@ -121,7 +121,7 @@
 						</div>
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
-								<li><a href="index.html" class="active">Home</a></li>
+								<li><a href="/" class="active">Home</a></li>
 								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
 									<ul role="menu" class="sub-menu">
 										<li><a href="shop.html">Products</a></li>
